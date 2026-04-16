547366b024120dc06db07d83ccd4fbceae86f633
N3STOR173 <N3STOR173@users.noreply.github.com>
2026-04-16 22:10:05 +0000

    deploy: inestrivino/Estadisticas-y-logros-Acepta-el-reto@9c4ceb81803458a71f599b21b2ba22533969dc6d

--- back/dev/src/servicios/usuarioService.ts
+++ back/dev/src/servicios/usuarioService.ts
@@ -1,9 +1,7 @@
-import UsuarioDAO from '../dao/usuarioDAO.js';
+import usuarioDAO from '../dao/usuarioDAO.js';
 import logros from "./logros/Logros.js";
 
-const usuarioDAO = new UsuarioDAO();
-
-export default class UsuarioService {
+class UsuarioService {
 
     getResultados(usuario: string) {
         return usuarioDAO.getResultados(usuario);
@@ -97,3 +95,5 @@ export default class UsuarioService {
         return usuarioDAO.getNumFranjasHorariasConEnvio(usuario);
     }
 }
+
+export default new UsuarioService();

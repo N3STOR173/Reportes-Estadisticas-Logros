df911e9bbd50460b45bc947de606ddf626c14db4
N3STOR173 <N3STOR173@users.noreply.github.com>
2026-04-22 10:38:01 +0000

    deploy: inestrivino/Estadisticas-y-logros-Acepta-el-reto@2a6211b2dcfe0df4cc1677de7f50e3384b484836

---
+++ front/main/src/pages/PruebaSocket.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+import PruebaSocketComponent from "../componentes/pruebaSocket"
+import PanelParticipacion from "../componentes/panelParticipacion"
+
+export default function PruebaSocket() {
+    return (
+        <div>
+            <p>Panel de pruebas</p>
+            <PruebaSocketComponent />
+            <Link to="/problemas/problema1">Get</Link>
+
+            <p>Panel de participacion</p>
+        </div>
+    )
+}

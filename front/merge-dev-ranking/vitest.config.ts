7604f5fa9a5abb6da442026988c045a4fff2c755
Néstor García Mayor <116187356+N3STOR173@users.noreply.github.com>
2026-04-23 00:36:31 +0200

    Delete front directory

--- front/merge-dev-ranking/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-    test: {
-        environment: "jsdom",
-        reporters: ['default', 'html'],
-        outputFile: './test-report/index.html'
-    },
-});
